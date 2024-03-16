092b6f2e8fee615576e4ea4667735e3fe98961b3
emodendy <143988852+emodendy@users.noreply.github.com>
2024-03-16 09:53:00 +0300

    Add files via upload

---
+++ BlazorApp1/BlazorApp1/Data/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+
+namespace BlazorApp1.Data
+{
+    public class Students
+    {
+        public Students(int iD, string surname, string name, string lastName, string group, string photo, DateTime dateOfBirth)
+        {
+            ID = iD;
+            Surname = surname;
+            Name = name;
+            LastName = lastName;
+            Group = group;
+            Photo = photo;
+            DateOfBirth = dateOfBirth;
+        }
+
+        public int ID { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Group { get; set; }
+        public string Photo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+    }
+    
+}
